11efcd2b6c9a954a3954067928745b5e9438622e
“Gmanlove” <emmagmanc@gmail.com>
2022-10-14 12:57:38 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,6 +17,8 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
 		putchar((digit1 % 10) + '0');
 		putchar((digit2 % 10) + '0');
 
@@ -27,6 +29,7 @@ int main(void)
 		putchar(' ');
 	}
 
+	}
 
 putchar('\n');
 
