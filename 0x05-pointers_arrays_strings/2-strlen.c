d30c98f67e8cd8e3f131d8a8657876d4f0a6d92f
“Gmanlove” <emmagmanc@gmail.com>
2022-10-24 08:03:33 -0700

    task1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/** 
- *  _strlen - returns the length of a string
- *   @s: string to evaluate
- *   Return: the length of the string
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
