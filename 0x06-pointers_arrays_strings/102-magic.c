2fe09c24987b0ddc16344f32c9725365a345671e
“Gmanlove” <emmagmanc@gmail.com>
2022-10-27 06:59:38 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,9 +12,9 @@ int main(void)
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
-	 *   - you are not allowed to modify p
-	 *   - only one statement
-	 *   - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
